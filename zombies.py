71ee0c71043cc2a6e53dbf577a46aab00b89a8ba
anatoleNSI <91608984+anatoleNSI@users.noreply.github.com>
2021-10-02 18:24:44 +0200

    debut class zombie

---
+++ zombies.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Zombies ():
+    def __init__(self) :
+       image = pygame.image.load('images\personages\Zombie_type_1.png') 
+       image = pygame.transform.scale(image, (100, 100)
+       screen.blit(int(pos[0]-size[0]/2), int(pos[1]-size[1]/2))) 
