05d3c62b3f528ddec244a3bf301cbc5d26ff0d4b
ChamaloDev <terence.desaintleger@gmail.com>
2021-10-03 14:18:50 +0200

    Bouton Pause !
    + Ajout d'un bouton pause/marche
    * Fix des fps
    * Amélioration du fonctionnement des trousses de soins

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from pygame import mouse
+from pygame.constants import MOUSEBUTTONDOWN, MOUSEBUTTONUP
+from pygame.event import wait
+from pygame.mixer import pause, unpause
 import intro
 import pygame
 import sys
@@ -48,14 +52,53 @@ def draw_rect(position, size, color) :
 	'''Permet de tracer un rectangle'''
 	pygame.draw.rect(screen, color, (position[0], position[1], size[0], size[1]))
 
+class Marche_Arret() :
+    def __init__(self) :
+        self.status = True
+        self.image = pygame.image.load('images\interface\Bouton_pause_stop.png')
+        self.size = 50
+        self.image = pygame.transform.scale(self.image,(self.size,self.size))
+        self.rect = self.image.get_rect()
+        self.rect.x = x - self.size*2
+        self.rect.y = 0
+
+    def display(self) :
+        screen.blit(self.image, self.rect)
+    
+    def highlight(self) :
+        '''Permet de faire briller le bouton pause quand on a sa souris dessus'''
+        pos = pygame.mouse.get_pos()
+        if pos[0] > x - self.size*2 and pos[1] < self.size*2 :
+            if self.status == True :
+                self.image = pygame.image.load('images\interface\Bouton_pause_stop_lumineux.png')
+            else :
+                self.image = pygame.image.load('images\interface\Bouton_pause_marche_lumineux.png')
+            return True
+        else :
+            if self.status == True :
+                self.image = pygame.image.load('images\interface\Bouton_pause_stop.png')
+            else :
+                self.image = pygame.image.load('images\interface\Bouton_pause_marche.png')
+            return False
+
+    def game_state(self) :
+        '''Pause ou Marche'''
+        return self.status
+
+    def on_off(self) :
+        '''Le setup qui permet de faire pause'''
+        if self.highlight() :
+            if self.status == True and pygame.mouse.get_pressed()[0] :
+                self.status = False
+                pygame.time.wait(100)
+            elif self.status == False and pygame.mouse.get_pressed()[0] :
+                self.status = True
+                pygame.time.wait(100)
+        
 class Score_actuel() :
     def __init__(self) :
         self.score = 0
         self.niveau = 5
-        self.points = 0
-        self.score += self.points
-        if self.points != 0 :
-            self.points = 0
         self.b_image = pygame.image.load('images/autres/Brouillard_V2.png')
         self.b_size_x = 1080+4240//self.niveau
         self.b_size_y = 720+2480//self.niveau
@@ -212,42 +255,48 @@ class Arme() :
         self.x = (x)+math.sin(convert_radians(angle))*100+50
         self.y = (y)+math.cos(convert_radians(angle))*100-10
         
-
 def main() :
+    marche_arret = Marche_Arret()
     grass = Grass()
     hero = Hero()
     score = Score_actuel()
     soin = Soin()
     while True :
-        dt = clock.tick(15)
+        dt = clock.tick(144)
         screen.fill(WHITE)
         for event in pygame.event.get() :
                     if event.type == pygame.QUIT :
                         pygame.quit()
                         sys.exit()
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP] or pressed[pygame.K_z] :
-            grass.bas(dt)
-            soin.bas(dt)
-        if pressed[pygame.K_DOWN] or pressed[pygame.K_s] :
-            grass.haut(dt)
-            soin.haut(dt)
-        if pressed[pygame.K_LEFT] or pressed[pygame.K_q] :
-            grass.gauche(dt)
-            soin.gauche(dt)
-        if pressed[pygame.K_RIGHT] or pressed[pygame.K_d] :
-            grass.droite(dt)
-            soin.droite(dt)
-        if soin.prendre() :
-            if hero.pv  <= 90 :
-                hero.pv += 10
-            else :
-                hero.pv = 100
-        hero.change(pygame.mouse.get_pos())
+        marche_arret.on_off()
+        if marche_arret.game_state() :
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_UP] or pressed[pygame.K_z] :
+                grass.bas(dt)
+                soin.bas(dt)
+            if pressed[pygame.K_DOWN] or pressed[pygame.K_s] :
+                grass.haut(dt)
+                soin.haut(dt)
+            if pressed[pygame.K_LEFT] or pressed[pygame.K_q] :
+                grass.gauche(dt)
+                soin.gauche(dt)
+            if pressed[pygame.K_RIGHT] or pressed[pygame.K_d] :
+                grass.droite(dt)
+                soin.droite(dt)
+            if soin.prendre() :
+                if hero.pv  < 65 :
+                    hero.pv += 35
+                elif hero.pv < 100 :
+                    hero.pv = 100
+                else :
+                    hero.pv = 100
+            hero.change(pygame.mouse.get_pos())
+            hero.pv -= 0.05 # Test de la bare de PV du héro, vu qu'il n'y a pas d'ennemies
         grass.display()
         soin.display()
         score.display()
         hero.display()
+        marche_arret.display()
         curseur()
         pygame.display.flip()
 
