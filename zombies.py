4fb75986132a0f6a2d9ef5b41cc4ed28a02ff800
anatoleNSI <91608984+anatoleNSI@users.noreply.github.com>
2021-10-06 15:49:05 +0200

    classe zombie : tentative de déplacement

--- zombies.py
+++ zombies.py
@@ -8,6 +8,8 @@ class Zombies ():
        image = pygame.transform.scale(image, (100, 100)
        screen.blit(int(pos[0]-size[0]/2), int(pos[1]-size[1]/2))) 
        self.size = 100
+       self.x = 0
+       self.y = 0
        self.pv = 30
        self.pv_maxi = 30
        self.rotated = pygame.image.load('images\personages\Zombie_type_1.png')
@@ -19,7 +21,17 @@ class Zombies ():
             self.pv = 0
 
     def spawn(self) :
+        self.x = 0
+        self.y = 0
         self.spawn = image()
+        self.spawn = (x, y)
 
+    def deplacement (self) :
+        if self.x > 540 and self.y > 360 :
+            self.x += 2 
+            self.y += 1
+        if self.x < 540 and self.y < 360 :
+            self.x -= 2 
+            self.y -= 1
 
         
