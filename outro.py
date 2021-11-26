47cbfaa82b7d106a55f545591f1fed8796ada13e
anatoleNSI <91608984+anatoleNSI@users.noreply.github.com>
2021-11-26 12:18:52 +0100

    erreur

--- outro.py
+++ outro.py
@@ -107,7 +107,7 @@ class outro():
 
 def main() :
     '''Boucle principale'''
-    outro = outro()
+    Outro = Outro()
     while True :
         for event in pygame.event.get() :
             if event.type == pygame.QUIT :
